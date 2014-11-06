737a4b08f7aa349996d8a218bb43230edc888b50
Audrey Jun <audrey.jun@gmail.com>
2014-11-06 12:58:48 -0800

    Added auto layout to Login and Signup pages

--- SignupViewController.m
+++ SignupViewController.m
@@ -26,14 +26,14 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 
 - (IBAction)signup:(id)sender {
@@ -71,6 +71,6 @@
             }
         }];
     }
-
+    
 }
 @end
